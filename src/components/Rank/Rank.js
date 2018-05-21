7855e111a4c65e3434a8d50d8537bc6361408a86
“Sam <sam.alsmadi@gmail.com>
2018-05-21 18:18:57 -0400

    Initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const rank = (props) => {
+  return (
+    <div>
+      <div className="white f3">
+        { 'Joe, your current rank is...' }
+      </div>
+
+      <div className="white f1">
+        {'#5'}
+      </div>
+    </div>
+  );
+};
+
+export default rank;

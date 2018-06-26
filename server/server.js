4d4aefc338e2937a2e1762b6e0b61b97db0f03ac
“Sam <sam.alsmadi@gmail.com>
2018-06-26 00:39:37 -0400

    refact: check autherization in headers during signin

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use(morgan('combined'));
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('it is working!'); });
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt); });
+app.post('/signin', (req, res) => { signin.signinAuthentication(db, bcrypt)(req, res); });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db); });
 app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db); });

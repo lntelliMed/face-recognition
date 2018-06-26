d8db902e931afb574680f48b02e39489c3aeaec2
“Sam <sam.alsmadi@gmail.com>
2018-06-26 19:35:29 -0400

    Add badging system using Serverless/AWS Lambda functions

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Face Recognition Brain: An app that will detect faces in your pictures.
 
-Technologies used include React, HTML5/CSS3, Tachyons, Node.js, Express, Knex/PostgreSQL, Clarifai API.
+Technologies used include React, HTML5/CSS3, Tachyons, Node.js, Express, Knex/PostgreSQL, Clarifai API, Docker, AWS Lambda/Serverless, Redis, JWT.
 
 [See this app live!](https://lntellimed.github.io/face-recognition/)
 
--- client/src/components/Rank/Rank.js
+++ client/src/components/Rank/Rank.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const rank = (props) => {
-  return (
-    <div >
-      <div className="white f3">
-        {`${props.name} , your current entry count is...`}
-      </div>
+class Rank extends Component {
+  state = {
+    emoji: ''
+  }
 
-      <div className='white f1 '>
-        {props.entries}
+  componentDidMount () {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://52rgvs0ih1.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+    .then(response => response.json())
+    .then(data => this.setState({ emoji: data.input }))
+    .catch(console.log);
+  }
+
+  render () {
+    return (
+      <div >
+        <div className="white f3">
+          {`${this.props.name} , your current entry count is...`}
+        </div>
+
+        <div className='white f1 '>
+          {this.props.entries}
+        </div>
+        <div className='white f3 '>
+          {`Rank Badge: ${this.state.emoji}`}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
-export default rank;
+export default Rank;
